ada9beada4bd55d2540f929893378664f1507101
Verah Mokaya <verahmokaya475@gmail.com>
2025-09-19 12:13:08 +0300

    mimicServer function careated to allow success message deplayed when clicked, and hidden class created to hide the erro after 3seconds when it failes using the catch function

--- main.js
+++ main.js
@@ -3,7 +3,27 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll('.like-glyph');
+const errorModal = document.getElementById('moddal');
 
+hearts.forEach(heart => {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then(() => {
+      // success handling will go here later
+      console.log("Server call succeeded");
+    })
+    .catch(()=> {
+      // show the modal
+      errorModal.classList.remove('hidden');
+
+      //hide it again after 3seconds
+      setTimeout(()=> {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+  });
+});
 
 
 
